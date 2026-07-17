f07f1b0e5652f4c61eb1d9365ed4cebe0e0ab08f
Hesler Cuevas <176766870+HeslerCuevas@users.noreply.github.com>
2026-07-17 00:51:19 -0400

    database timeout handling

--- app/db/database.py
+++ app/db/database.py
@@ -9,7 +9,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(
     DATABASE_URL,
     echo=False,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    connect_args={
+        "login_timeout": 10,  # Fails fast within 10 seconds if Azure SQL takes too long to authenticate
+        "timeout": 10        # Fails fast if any individual query gets blocked or hangs
+    }
 )
 
 def get_session():
